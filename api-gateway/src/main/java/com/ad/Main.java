6135aafca17094bb12cb25705b3e1e069a20ec52
Pavel <3211232133@mail.ru>
2026-02-14 18:57:12 +0300

    Пересмотр архитектуры

---
+++ api-gateway/src/main/java/com/ad/Main.java
@@ -0,0 +1,11 @@
+package com.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main{
+    public static void main(String[] args){
+        new SpringApplication(Main.class);
+    }
+}
---
+++ collector-service/src/main/java/com/ad/Main.java
@@ -0,0 +1,5 @@
+package com.ad;
+
+public class Main {
+
+}
